c454cf7e633757fbb75a232045db8817c8c75e90
Deepak Ramani <deepakramani65@gmail.com>
2024-09-01 15:31:21 +0530

    first commit

---
+++ app/api/users/[id]/posts/route.js
@@ -0,0 +1,18 @@
+import { connectedToDB } from "@utils/database";
+import Prompt from "@models/prompt";
+
+export const GET = async (request, { params }) => {
+    try {
+        await connectedToDB();
+
+        const prompts = await Prompt.find({
+            creator: params.id
+        }).populate('creator');
+
+        return new Response(JSON.stringify(prompts), {
+            status: 200 })
+    } catch (error) {
+        return new Response("Failed to fetch all prompts", {
+            status: 500 })
+    }
+}
